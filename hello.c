6443356fa28ed961455848138313e0a4a68008c1
strattonsteven <42447714+strattonsteven@users.noreply.github.com>
2018-08-23 12:58:42 -0500

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Steven Stratton
+ * Date: 2018/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Steven Stratton Computer Science\n");
+
+  return 0;
+}
